a9bcb19508452a612e6ac09819bc847c5262315b
LittleFish-Coder <adrianyu890317@gmail.com>
2024-06-06 03:16:22 +0800

    update net_D url

--- scripts/download_pix2pix_pretrained.sh
+++ scripts/download_pix2pix_pretrained.sh
@@ -5,7 +5,7 @@ mkdir -p ./checkpoints/fish_pix2pix
 NET_G_FILE=./checkpoints/fish_pix2pix/latest_net_G.pth
 NET_D_FILE=./checkpoints/fish_pix2pix/latest_net_D.pth
 net_G_url="https://ncku365-my.sharepoint.com/:u:/g/personal/nm6121030_ncku_edu_tw/Eede7zJZ2xpCroTGVxtyfDcB1QNLo9stAWBGJcTrdHKByw?e=FzqNAJ&download=1"
-net_D_url="https://ncku365-my.sharepoint.com/:u:/g/personal/nm6121030_ncku_edu_tw/Eede7zJZ2xpCroTGVxtyfDcB1QNLo9stAWBGJcTrdHKByw?e=HpyDdf&download=1"
+net_D_url="https://ncku365-my.sharepoint.com/:u:/g/personal/nm6121030_ncku_edu_tw/ESaJ5Fy4ZlNBtoxhREusS34BmZy2f72PJN3Zw7hR-E2khg?e=JpR5es&download=1"
 
 # download the pretrained weight
 echo "Downloading the pretrained weight for the fish_pix2pix model..."
