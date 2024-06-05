75bdf5ed80f2fbc57567bcd748d8306ae76380be
Chen-Yang Yu <adrianyu890317@gmail.com>
2024-06-05 20:04:56 +0800

    update url

--- scripts/download_pix2pixHD_pretrained.sh
+++ scripts/download_pix2pixHD_pretrained.sh
@@ -4,8 +4,8 @@ mkdir -p ./checkpoints/fish_pix2pix
 # set the model file path
 NET_G_FILE=./checkpoints/fish_pix2pix/latest_net_G.pth
 NET_D_FILE=./checkpoints/fish_pix2pix/latest_net_D.pth
-net_G_url="https://ncku365-my.sharepoint.com/:u:/g/personal/nm6121030_ncku_edu_tw/Ef77KmFwjntMjxbKCke8UooB5WOo9tjKbtTsZxPkmH0PPQ?e=HydvmT&download=1"
-net_D_url="https://ncku365-my.sharepoint.com/:u:/g/personal/nm6121030_ncku_edu_tw/Ef77KmFwjntMjxbKCke8UooB5WOo9tjKbtTsZxPkmH0PPQ?e=7ZtfFN&download=1"
+net_G_url="https://ncku365-my.sharepoint.com/:u:/g/personal/nm6121030_ncku_edu_tw/Ef77KmFwjntMjxbKCke8UooB5WOo9tjKbtTsZxPkmH0PPQ?e=Rk9KQ3&download=1"
+net_D_url="https://ncku365-my.sharepoint.com/:u:/g/personal/nm6121030_ncku_edu_tw/EWrrWqrJ4BlJmj8ixkIHYA8BHgjPRXY1zFPKtsjNviHMrA?e=EwgJa9&download=1"
 
 # download the pretrained weight
 echo "Downloading the pretrained weight for the fish_pix2pixHD model..."
